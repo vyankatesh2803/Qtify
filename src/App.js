ae21311344a425523d837443b72c674c4b52ccd8
vyankatesh2803 <vyankatesh2803@gmail.com>
2023-11-16 19:14:45 +0530

    hero-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar.js';
+import Hero from './Components/HeroSection/Hero.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/HeroSection/Hero.js
@@ -0,0 +1,13 @@
+import './Hero.css'
+
+export default () => {
+    return <div className="hs-container">
+                <div className="hs-flex">
+                    <div className="headline">
+                        <h1>100 Thousand Songs, ad-free</h1>
+                        <h1>Over thousand podcast episodes</h1>
+                    </div>
+                    <img src="HeadphoneLogo.png" className="logo"></img>
+                </div>
+            </div>
+}
